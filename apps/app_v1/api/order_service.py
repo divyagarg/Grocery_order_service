1d667341ac3b8d64a74a0c907652e6ea802670c0
amit.bansal <amit.asr@gmail.com>
2016-06-02 12:38:32 +0530

    master_order_billing_address

--- apps/app_v1/api/order_service.py
+++ apps/app_v1/api/order_service.py
@@ -554,12 +554,24 @@ class OrderService:
 		order.payment_mode = self.payment_mode
 		order.status_id = StatusService.get_status_id(ORDER_STATUS.APPROVED_STATUS.value) if self.payment_mode == payment_modes_dict[0]\
 																						  else StatusService.get_status_id(ORDER_STATUS.PENDING_STATUS.value)
+
 		if self.billing_address is not None:
 			billing_address = self.billing_address
 			address = Address.get_address(billing_address['name'], billing_address['mobile'],
 										  billing_address['address'], billing_address['city'],
 										  billing_address['pincode'], billing_address['state'],
 										  billing_address.get('email'), billing_address.get('landmark'))
+			order.billing_address_ref = address.address_hash
+		elif self.cart_reference_given:
+			order.billing_address_ref = self.shipping_address
+		else:
+			shipping_address = self.shipping_address
+
+			address = Address.get_address(shipping_address['name'], shipping_address['mobile'],
+										  shipping_address['address'], shipping_address['city'],
+										  shipping_address['pincode'], shipping_address['state'],
+										  shipping_address.get('email'), shipping_address.get('landmark'))
+
 			order.billing_address_ref = address.address_hash
 
 		db.session.add(order)
